c379b586210efac3066479f89b90eea7a85dff1b
Paolo Compagnoni <compagnonipaolo95@gmail.com>
2022-11-05 02:29:57 +0100

    Stories' design done

---
+++ data/users.js
@@ -0,0 +1,26 @@
+export const USERS = [
+    {
+        user: 'flowers',
+        image: 'https://images.pexels.com/photos/56866/garden-rose-red-pink-56866.jpeg?cs=srgb&dl=pexels-pixabay-56866.jpg&fm=jpg&_gl=1*mh6rmk*_ga*NDU1NzY3ODk1LjE2Njc2MTA2NTg.*_ga_8JE65Q40S6*MTY2NzYxMDY1OC4xLjEuMTY2NzYxMDk3OC4wLjAuMA..',
+    },
+    {
+        user: 'art_pic',
+        image: 'https://images.pexels.com/photos/2071150/pexels-photo-2071150.jpeg?cs=srgb&dl=pexels-matheus-henrin-2071150.jpg&fm=jpg&_gl=1*ujl074*_ga*NDU1NzY3ODk1LjE2Njc2MTA2NTg.*_ga_8JE65Q40S6*MTY2NzYxMDY1OC4xLjEuMTY2NzYxMDg2OC4wLjAuMA..',
+    },
+    {
+        user: 'dogslover',
+        image: 'https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?cs=srgb&dl=pexels-chevanon-photography-1108099.jpg&fm=jpg&_gl=1*1v4b05d*_ga*NDU1NzY3ODk1LjE2Njc2MTA2NTg.*_ga_8JE65Q40S6*MTY2NzYxMDY1OC4xLjEuMTY2NzYxMDk5OC4wLjAuMA..',
+    },
+    {
+        user: 'alfa_romeo',
+        image: 'https://images.pexels.com/photos/8711280/pexels-photo-8711280.jpeg?cs=srgb&dl=pexels-eathan-hood-8711280.jpg&fm=jpg&_gl=1*axu43x*_ga*NDU1NzY3ODk1LjE2Njc2MTA2NTg.*_ga_8JE65Q40S6*MTY2NzYxMDY1OC4xLjEuMTY2NzYxMDgzOS4wLjAuMA..',
+    },
+    {
+        user: 'scuderiaferrari',
+        image: 'https://images.pexels.com/photos/13641535/pexels-photo-13641535.jpeg?cs=srgb&dl=pexels-jenda-kube%C5%A1-13641535.jpg&fm=jpg&_gl=1*1kkj9z7*_ga*NDU1NzY3ODk1LjE2Njc2MTA2NTg.*_ga_8JE65Q40S6*MTY2NzYxMDY1OC4xLjEuMTY2NzYxMDc5My4wLjAuMA..',
+    },
+    {
+        user: 'ferrari',
+        image: 'https://images.pexels.com/photos/404190/pexels-photo-404190.jpeg?cs=srgb&dl=pexels-jamil-rostum-404190.jpg&fm=jpg&_gl=1*s59sy0*_ga*NDU1NzY3ODk1LjE2Njc2MTA2NTg.*_ga_8JE65Q40S6*MTY2NzYxMDY1OC4xLjEuMTY2NzYxMDY3NS4wLjAuMA..',
+    },
+]
