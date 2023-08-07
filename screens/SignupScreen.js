f84ece295be7ea62b167635a9661fcde093bf66c
Paolo Compagnoni <compagnonipaolo95@gmail.com>
2023-08-08 00:39:31 +0200

    SignupForm design done

---
+++ screens/SignupScreen.js
@@ -0,0 +1,29 @@
+import { View, StyleSheet, Image } from 'react-native'
+import React from 'react'
+import SignupForm from '../components/signupScreen/SignupForm'
+
+const INSTAGRAM_LOGO = "https://img.icons8.com/fluency/400/instagram-new.png"
+
+const SignupScreen = () => (
+    <View style={styles.container}>
+      <View style={styles.logoContainer}>
+        <Image source={{ uri: INSTAGRAM_LOGO, height: 100, width: 100 }}/>
+      </View>
+      <SignupForm />
+    </View>
+)
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        paddingTop: 50,
+        paddingHorizontal: 12,
+    },
+    logoContainer: {
+        alignItems: 'center',
+        marginTop: 60,
+    }
+})
+
+export default SignupScreen
